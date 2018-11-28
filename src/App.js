c8e8546852e78856f5945199b92aa8e241ba6c1b
Noah <swimnoah113@gmail.com>
2018-11-27 22:04:42 -0500

    Made visual and functional changes to the UI

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ import { GMap } from 'primereact/gmap';
 import { Dropdown } from 'primereact/dropdown';
 import { Card } from 'primereact/card';
 import { Rating } from 'primereact/rating';
+import { Button } from 'primereact/button';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { InputText } from 'primereact/inputtext';
 
 const mapStyles = {
     width: '100%',
@@ -54,15 +57,30 @@ class App extends Component {
         super();
 
         this.state = {
-            building: null
+            building: null,
+            comment: '',
+            title: ''
         };
 
         this.buildingChanged = this.buildingChanged.bind(this);
+        this.makePost = this.makePost.bind(this);
 
     }
 
     buildingChanged(e) {
         this.setState({building: e.value});
+    }
+
+    makePost(e) {
+        curr_location_data_fake.comments.push(
+            {
+                author: 'Test',
+                title: this.state.title,
+                rating: this.state.review,
+                desc: this.state.comment
+            }
+        );
+        this.forceUpdate();
     }
 
     createComments = () => {
@@ -75,10 +93,10 @@ class App extends Component {
                 <div className="p-col-12">
                     <Card title={comment.title} subTitle={comment.author}>
                         <div className="p-grid">
-                            <div className="p-col-12">
+                            <div className="p-col-12" style={{'text-align': 'left'}}>
                                 {comment.desc}
                             </div>
-                            <div className="p-col-12">
+                            <div className="p-col-12" style={{'text-align': 'left'}}>
                                 <Rating value={comment.rating} readonly={true} stars={10} cancel={false} />
                             </div>
                         </div>
@@ -93,22 +111,54 @@ class App extends Component {
         return comments;
     }
 
+    maybeAllowNewComment = () => {
+        let comment = [];
+        // if (userAuthenticated) {
+            comment.push(
+                    <div className="p-col-12" style={{'text-align': 'left'}}>
+                        <h4>Comment Title:</h4> 
+                        <InputText value={this.state.title} onChange={(e) => this.setState({title: e.target.value})} />
+                    </div>
+            );
+            comment.push(
+                    <div className="p-col-12" style={{'text-align': 'left'}}>
+                        <h4>Comment Title:</h4> 
+                        <InputTextarea rows={5} cols={30} value={this.state.comment} autoResize={true} onChange={(e) => {this.setState({comment: e.target.value})}} />
+                    </div>
+            );
+            comment.push(
+                    <div className="p-col-12" style={{'text-align': 'left'}}>
+                        <h4>Give Review:</h4> 
+                        <Rating value={this.state.review} stars={10} cancel={false} onChange={(e) => {this.setState({review: e.target.value})}} />
+                    </div>
+            );
+            comment.push(
+                    <div className="p-col-12" style={{'text-align': 'left'}}>
+                        <Button label="Post Comment" onClick={this.makePost}/>
+                    </div>
+            );
+            return comment;
+        // } else {
+
+        // }
+    }
+
     render() {
         return (
             <div className="App">
                 <div className="p-grid p-col-12">
-                    <div className="p-col-8">
+                    <div className="p-col-6">
                         <GMap options={{
                                 center: {lat: 40.4318914, lng: -86.91750952604869},
                                 zoom: 14
                             }} style={mapStyles}
                         />
                     </div>
-                    <div className="p-col-4">
-                        <Card style={{'width': '100%', 'height': '100%'}}>
+                    <div className="p-col-3">
+                        <Card style={{'width': '100%', 'height': '100%', 'text-align': 'left'}} title={"Study Space"}>
                             <div className="p-grid">
-                                <div className="p-col-12">
-                                    Select Building:
+                                <div className="p-col-12" style={{'text-align': 'left'}}>
+                                    <h4>Select Building:</h4>
                                     <Dropdown style={{'width': '150px'}} 
                                         value={this.state.building} 
                                         options={locations} 
@@ -116,25 +166,31 @@ class App extends Component {
                                         placeholder="Select a Building"
                                     />
                                 </div>
-                                <div className="p-col-12">
-                                    Building Information:
+                                <div className="p-col-12" style={{'text-align': 'left'}}>
+                                    <h4>Building Information:</h4>
                                 </div>
-                                <div className="p-col-12">
-                                    Name: {curr_location_data_fake.name} ({curr_location_data_fake.short_name})
-                                </div>
-                                
-                                <div className="p-col-12">
-                                    Comments:
+                                <div className="p-col-12" style={{'text-align': 'left'}}>
+                                    <h4>Name:</h4> 
+                                    {curr_location_data_fake.name} ({curr_location_data_fake.short_name})
                                 </div>
                                 
-                                {this.createComments()}
+                                <div className="p-col-12" style={{'text-align': 'left'}}>
+                                    <h4>Comments:</h4>
+                                    {this.createComments()}
+                                </div>
 
-                                <div className="p-col-12">
-                                    Average rating:
+                                <div className="p-col-12" style={{'text-align': 'left'}}>
+                                    <h4>Average rating:</h4>
                                     <Rating value={curr_location_data_fake.average_review} readonly={true} stars={10} cancel={false} />
-                                </div>
+                                </div>         
+                                
                             </div>
 
+                        </Card>
+                    </div>
+                    <div className="p-col-3">
+                        <Card style={{'width': '100%', 'text-align': 'left'}} title={"Give a Review"}>
+                            {this.maybeAllowNewComment()} 
                         </Card>
                     </div>
                 </div>
