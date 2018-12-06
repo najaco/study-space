fc912a8262dff97d469f732e71a0b87cd2201860
Nathan <cohen53@purdue.edu>
2018-12-06 17:46:25 -0500

    Updates

--- precache-manifest.f67d5b193636c29caa4cfc5f87c1b409.js
+++ precache-manifest.f67d5b193636c29caa4cfc5f87c1b409.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
-    "url": "/study-space/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/study-space/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
   },
   {
-    "revision": "fc800611d670dff891f6",
+    "revision": "bbc59ca07868f7e74151",
     "url": "/study-space/static/css/main.16f7b17a.chunk.css"
   },
   {
@@ -20,24 +20,24 @@ self.__precacheManifest = [
     "url": "/study-space/static/js/runtime~main.dc782c92.js"
   },
   {
-    "revision": "60c866748ff15f5b347fdba64596b1b1",
-    "url": "/study-space/static/media/open-sans-v15-latin-300.60c86674.woff2"
-  },
-  {
     "revision": "76b56857ebbae3a5a689f213feb11af0",
     "url": "/study-space/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/study-space/static/media/open-sans-v15-latin-300.60c86674.woff2"
   },
   {
     "revision": "9dce7f01715340861bdb57318e2f3fdc",
     "url": "/study-space/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
   },
   {
-    "revision": "148a6749baa5f658a45183ddb5ee159f",
-    "url": "/study-space/static/media/open-sans-v15-latin-700.148a6749.eot"
-  },
-  {
     "revision": "cffb686d7d2f4682df8342bd4d276e09",
     "url": "/study-space/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/study-space/static/media/open-sans-v15-latin-700.148a6749.eot"
   },
   {
     "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
@@ -48,44 +48,44 @@ self.__precacheManifest = [
     "url": "/study-space/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
   },
   {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/study-space/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "bbc59ca07868f7e74151",
+    "url": "/study-space/static/js/main.bbc59ca0.chunk.js"
+  },
+  {
     "revision": "521d17bc9f3526c690e8ada6eee55bec",
     "url": "/study-space/static/media/open-sans-v15-latin-300.521d17bc.woff"
   },
   {
-    "revision": "fc800611d670dff891f6",
-    "url": "/study-space/static/js/main.fc800611.chunk.js"
-  },
-  {
-    "revision": "7e08cc656863d52bcb5cd34805ac605b",
-    "url": "/study-space/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/study-space/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
   },
   {
     "revision": "177cc92d2e8027712a8c1724abd272cd",
     "url": "/study-space/static/media/open-sans-v15-latin-300.177cc92d.ttf"
   },
   {
-    "revision": "623e3205570002af47fc2b88f9335d19",
-    "url": "/study-space/static/media/open-sans-v15-latin-700.623e3205.woff"
-  },
-  {
     "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
     "url": "/study-space/static/media/open-sans-v15-latin-300.27ef0b06.svg"
-  },
-  {
-    "revision": "7aab4c13671282c90669eb6a10357e41",
-    "url": "/study-space/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
   },
   {
     "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
     "url": "/study-space/static/media/open-sans-v15-latin-700.2e00b263.svg"
   },
   {
-    "revision": "567f57385ea3dde2c9aec797d07850d2",
-    "url": "/study-space/static/media/line.567f5738.gif"
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/study-space/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
   },
   {
     "revision": "c7a33805ffda0d32bd2a9904c8b02750",
     "url": "/study-space/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/study-space/static/media/line.567f5738.gif"
   },
   {
     "revision": "71bb3d79dcf18b45ae845409e7c2ada3",
@@ -104,7 +104,7 @@ self.__precacheManifest = [
     "url": "/study-space/static/css/1.c8d2622a.chunk.css"
   },
   {
-    "revision": "e9d9f7262aea879c0a3858c15cdef281",
+    "revision": "3221eab0f7e7db0d2944019c6e5fcec0",
     "url": "/study-space/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/study-space/precache-manifest.adeb6a6ac30265de0ff85583c47037bc.js"
+  "/study-space/precache-manifest.f67d5b193636c29caa4cfc5f87c1b409.js"
 );
 
 workbox.clientsClaim();
